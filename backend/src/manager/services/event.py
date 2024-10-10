ba823685591d62db2fa11b3611ba80a400489646
hpxix <108882735+hpxix@users.noreply.github.com>
2024-10-10 22:17:52 +0300

    [add] enum value comparison to update -> update_charge_point_status

--- backend/src/manager/services/event.py
+++ backend/src/manager/services/event.py
@@ -1,18 +1,28 @@
 from aio_pika.abc import AbstractIncomingMessage
 from loguru import logger
 import json
-
+from charge_point_node.main import OnConnectionMessage, EventName
 # from charge_point_node.main import OnconnectionEvent, EventName
 import logging
+from manager.services.charge_point import update_charge_point_status
 
 logging.basicConfig(level=logging.INFO)
 
 
-async def process_event(event: AbstractIncomingMessage) -> None:
+async def process_event(message: AbstractIncomingMessage) -> None:
     # Use 'async with' context manager for processing the event
-    async with event.process():
-        # Decode the event body and store it as a string
-        event_body = event.body.decode()
-
+    async with message.process():
+        # Decode the message body and store it as a string and convert to json
+        data = json.loads(message.body.decode())
+        event_name = data["name"]
+        # value because we have a value as a string and not an Enum object and map it to OnConnectionMessages
+        event = {
+            EventName.NEW_CONNECTION.value: OnConnectionMessage
+        }[event_name](**data)
         # Log the decoded event
-        logger.info(f"Got Events from charge point node (event={event_body})")
+        logger.info(f"Got Events from charge point node (event={event})")
+
+        # we need an Enum value because we need to compare objects with is operator and not comparison double equal ==
+        if event.name is EventName.NEW_CONNECTION:
+            status = "online"
+            await update_charge_point_status(charge_point_id=event.charge_point_id, status=status)
