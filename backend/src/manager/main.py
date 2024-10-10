aeaff72756883dc86d8f082867089940a0b069c7
hpxix <108882735+hpxix@users.noreply.github.com>
2024-10-10 22:15:55 +0300

    [change] Fast api config

--- backend/src/manager/main.py
+++ backend/src/manager/main.py
@@ -4,15 +4,18 @@ from core.queue.consumer import start_consume
 from core.settings import EVENT_QUEUE_NAME
 from manager.controllers.status import status_router
 from manager.services.event import process_event
+from sse.controller import stream_router
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
+
+app = FastAPI()
 
 
 asyncio.ensure_future(
     start_consume(EVENT_QUEUE_NAME, on_message=process_event)
 )
 
-app = FastAPI()
 
 app.include_router(status_router)
+
